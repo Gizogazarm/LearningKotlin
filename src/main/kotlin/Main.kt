3a630c3c265298012c2f452e209427939248098f
Gizogazarm <theblues11.helmi@gmail.com>
2023-02-28 06:32:41 +0700

    membuat file main untuk jalan program

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main () {
+
+    println("==========================")
+    println("GAME SUIT TERMINAL VERSION")
+    println("==========================")
+    println("1. Masukkan Pemain 1 :")
+    println("2. Masukkan Pemain 2 :")
+
+
+}
