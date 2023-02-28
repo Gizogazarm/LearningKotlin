1228c15d2d98070def7c255e2783749c54560637
Gizogazarm <theblues11.helmi@gmail.com>
2023-02-28 19:16:33 +0700

    menambahkan fun suit untuk menilai suit tersebut benar atau tidak dan implementasi use suit dan tes suit dan penerapan pada main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
 
+    val suit = arrayOf("gunting", "batu","kertas")
     val namaPemain1 = "Pemain 1"
     val namaPemain2 = "Pemain 2"
     val player1 = Player()
@@ -12,8 +13,10 @@ fun main() {
     println("==========================")
     println("1. Masukkan ${player1.getName()} :")
     val inputanPemain1 = readLine()?.lowercase()!!
+    player1.tesSuit(player1.useSuit(inputanPemain1,suit))
     println("2. Masukkan ${player2.getName()} :")
     val inputanPemain2 = readLine()?.lowercase()!!
+    player2.tesSuit(player2.useSuit(inputanPemain2,suit))
 
 
 }
--- src/main/kotlin/Suit.kt
+++ src/main/kotlin/Suit.kt
@@ -1,12 +1,26 @@
 open class Suit {
-    val suit = arrayOf("gunting", "batu", "kertas")
 
-    fun useSuit(jurusUser: String) {
+    private val salah = "Mohon maaf ulangi dengan benar"
+    private var nilai = false
+
+    fun useSuit(jurusUser: String, suit: Array<String>):Boolean {
         for (i in 0 until suit.size) {
-
+            if (jurusUser == suit[i]) {
+                nilai = true
+                break
+            } else {
+                nilai = false
+            }
         }
-
+        return nilai
     }
 
+    fun tesSuit(statusSuit: Boolean) {
+        if(statusSuit == true) {
+            println("bagus")
+        } else {
+            println(salah)
+        }
+    }
 
 }
