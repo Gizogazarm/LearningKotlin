90e017efee80309727817e7d3a441448406df9de
Gizogazarm <theblues11.helmi@gmail.com>
2023-02-28 07:55:54 +0700

    menambahkan fun getname dan status pemain pada class player dan implementasi main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 fun main () {
 
+    val namaPemain1 = "Pemain 1"
+    val namaPemain2 = "Pemain 2"
+    val player1 = Player(namaPemain1)
+    val player2 = Player(namaPemain2)
+
+
     println("==========================")
     println("GAME SUIT TERMINAL VERSION")
     println("==========================")
-    println("1. Masukkan Pemain 1 :")
-    println("2. Masukkan Pemain 2 :")
+    println("1. Masukkan ${player1.getName()} :")
+    println("2. Masukkan ${player2.getName()} :")
 
 
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,2 +1,16 @@
-class Player {
+class Player(namaPemain: String) {
+    private var status = false
+    private val namaPlayer = namaPemain
+
+    fun statusPemain() {
+        if (this.status == true) {
+            println("Pemain $namaPlayer MENANG !!")
+        } else {
+            println("Pemain $namaPlayer KALAH !!")
+        }
+    }
+
+    fun getName(): String {
+        return this.namaPlayer
+    }
 }
