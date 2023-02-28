e1333a7a7e83cc0f5e3c8dc56443a49f737274c4
Gizogazarm <theblues11.helmi@gmail.com>
2023-02-28 17:00:39 +0700

    implementasi setname pada main.kt dan update pada main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,10 @@ fun main() {
 
     val namaPemain1 = "Pemain 1"
     val namaPemain2 = "Pemain 2"
-    val player1 = Player(namaPemain1)
-    val player2 = Player(namaPemain2)
-
-
+    val player1 = Player()
+    val player2 = Player()
+    player1.setName(namaPemain1)
+    player2.setName(namaPemain2)
 
     println("==========================")
     println("GAME SUIT TERMINAL VERSION")
