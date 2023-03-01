4be17fd695865b5226c81b9dab8551d43248623d
Gizogazarm <theblues11.helmi@gmail.com>
2023-03-01 17:24:46 +0700

    menghapus fun statusplayer pada class Player

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,17 +1,9 @@
-class Player:Suit() {
+class Player():Suit() {
     private var status = false
     private var namaPlayer: String? = ""
 
     fun nilaiAwalPlayer(): Boolean {
         return nilai
-    }
-
-    fun statusPemain() {
-        if (this.status == true) {
-            println("Pemain $namaPlayer MENANG !!")
-        } else {
-            println("Pemain $namaPlayer KALAH !!")
-        }
     }
 
     fun setName(namaPemain: String) {
