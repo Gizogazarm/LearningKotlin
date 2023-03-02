991c249db973c6e85b08a4e746a017d0adae8e0c
Gizogazarm <theblues11.helmi@gmail.com>
2023-03-02 09:57:17 +0700

    penambahan variabel penjelasan untuk pilihan suit pada file main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
 
     val suit = arrayOf("gunting", "batu", "kertas")
+    val penjelasan = "Pilihan Suit : Gunting, Batu , Kertas ( Masukkan huruf dengan benar tanpa spasi)"
     val namaPemain1 = "Pemain 1"
     val namaPemain2 = "Pemain 2"
     val player1 = Player()
@@ -12,9 +13,11 @@ fun main() {
     println("==========================")
     println("GAME SUIT TERMINAL VERSION")
     println("==========================")
-    println("Pilihan Suit : Gunting, Batu , Kertas")
+
     while (player1.kondisiSuit() == false) {
         println("1. Masukkan ${player1.getName()} :")
+        println(penjelasan)
+        println("==========================")
         val inputanPemain1 = readLine()?.lowercase()!!
         player1.tesSuit(player1.useSuit(inputanPemain1, suit))
     }
@@ -22,6 +25,8 @@ fun main() {
 
     while (player2.kondisiSuit() == false) {
         println("2. Masukkan ${player2.getName()} :")
+        println(penjelasan)
+        println("==========================")
         val inputanPemain2 = readLine()?.lowercase()!!
         player2.tesSuit(player2.useSuit(inputanPemain2, suit))
     }
